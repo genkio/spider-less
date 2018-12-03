ec8bcfdcfb5e28d66e3091caef2dc285b67c3eee
slashbit <5327840+slashbit@users.noreply.github.com>
2018-12-04 00:46:37 +0900

    create aws sdk wrapper module

---
+++ src/clients/aws.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.REGION })
+
+const dynamodb = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true })
+const { TABLE_SUBSCRIPTIONS } = process.env
+
+function create (item, table = TABLE_SUBSCRIPTIONS) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: table,
+      Item: item
+    }
+    dynamodb.put(params, error => {
+      if (error) return reject(error)
+      resolve(item)
+    })
+  })
+}
+
+function remove (id, table = TABLE_SUBSCRIPTIONS) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: table,
+      Key: { id }
+    }
+    dynamodb.delete(params, (error, response) => {
+      if (error) return reject(error)
+      resolve(response)
+    })
+  })
+}
+
+function list (table = TABLE_SUBSCRIPTIONS) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: table
+    }
+    dynamodb.scan(params, (error, response) => {
+      if (error) return reject(error)
+      resolve(response.Items)
+    })
+  })
+}
+
+function publish (arn, message) {
+  return new Promise((resolve, reject) => {
+    new AWS.SNS().publish({
+      TopicArn: arn,
+      Message: message
+    }, (error, data) => {
+      if (error) return reject(error)
+      resolve(data)
+    })
+  })
+}
+
+module.exports = {
+  dynamodb: {
+    create,
+    remove,
+    list
+  },
+  sns: {
+    publish
+  }
+}
